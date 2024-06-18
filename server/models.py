1bc164b0bdc70e9c31a5bf4f9e68404447b283c7
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-06-18 10:30:33 -0500

    Add classes and join tables to models.py

--- server/models.py
+++ server/models.py
@@ -6,3 +6,126 @@ from sqlalchemy import ForeignKey
 from re import search
 
 from config import db, bcrypt
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=True, nullable=False)
+    last_name = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(60), nullable=False)
+
+class Role(db.Model, SerializerMixin):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+
+class Course(db.Model, SerializerMixin):
+    __tablename__ = 'courses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    discipline_id = db.Column(db.Integer, db.ForeignKey('disciplines.id'), nullable=False)
+    level_id = db.Column(db.Integer, db.ForeignKey('levels.id'), nullable=False)
+
+class Discipline(db.Model, SerializerMixin):
+    __tablename__ = 'disciplines'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+
+class Level(db.Model, SerializerMixin):
+    __tablename__ = 'levels'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+
+class Student(db.Model, SerializerMixin):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    secondary_email = db.Column(db.String(20), nullable=True)
+    birth_date = db.Column(db.Date, nullable=False)
+    gender_id = db.Column(db.Integer, db.ForeignKey('genders.id'), nullable=False)
+
+class Gender(db.Model, SerializerMixin):
+    __tablename__ = 'genders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+
+class StudentReport(db.Model, SerializerMixin):
+    __tablename__ = 'student_reports'
+
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    approved = db.Column(db.Boolean, nullable=False)
+
+class CourseReport(db.Model, SerializerMixin):
+    __tablename__ = 'course_reports'
+
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    approved = db.Column(db.Boolean, nullable=False)
+
+class Placement(db.Model, SerializerMixin):
+    __tablename__ = 'placements'
+
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+class Suggestion(db.Model, SerializerMixin):
+    __tablename__ = 'suggestions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=True)
+    discipline_id = db.Column(db.Integer, db.ForeignKey('disciplines.id'), nullable=True)
+    level_id = db.Column(db.Integer, db.ForeignKey('levels.id'), nullable=True)
+    gender_id = db.Column(db.Integer, db.ForeignKey('genders.id'), nullable=True)
+
+class Templates(db.Model, SerializerMixin):
+    __tablename__ = 'templates'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+class Emails(db.Model, SerializerMixin):
+    __tablename__ = 'emails'
+
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    email_address = db.Column(db.String(20), nullable=False)
+    secondary_email_address = db.Column(db.String(20), nullable=True)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+users_roles = db.Table('users_roles',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('roles.id'))
+)
+
+users_courses = db.Table('users_courses',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('courses.id'))
+)
+
+students_courses = db.Table('students_courses',
+    db.Column('student_id', db.Integer, db.ForeignKey('students.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('courses.id'))
+)
