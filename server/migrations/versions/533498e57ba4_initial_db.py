b4be5e2fd49ca941f5822a79aa10f934d0c65b2b
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-06-18 09:52:46 -0500

    initial commit

---
+++ server/migrations/versions/533498e57ba4_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 533498e57ba4
+Revises: 
+Create Date: 2024-06-18 09:52:34.136600
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '533498e57ba4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
