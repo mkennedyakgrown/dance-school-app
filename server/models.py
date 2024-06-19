73e888c4b463e530ca000a42696731028db91c9d
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-06-19 13:16:13 -0500

    fix Model relationship conflicts, add seed data

--- server/models.py
+++ server/models.py
@@ -16,10 +16,10 @@ class User(db.Model, SerializerMixin):
     email_address = db.Column(db.String(20), unique=True, nullable=False)
     password_hash = db.Column(db.String(60), nullable=False)
 
-    roles = db.relationship('Role', secondary='users_roles', backref='users')
-    courses = db.relationship('Course', secondary='users_courses', backref='users')
-    student_reports = db.relationship('StudentReport', backref='user')
-    course_reports = db.relationship('CourseReport', backref='user')
+    roles = db.relationship('Role', secondary='users_roles', back_populates='users')
+    courses = db.relationship('Course', secondary='users_courses', back_populates='users')
+    student_reports = db.relationship('StudentReport', back_populates='user')
+    course_reports = db.relationship('CourseReport', back_populates='user')
 
 class Role(db.Model, SerializerMixin):
     __tablename__ = 'roles'
@@ -28,7 +28,7 @@ class Role(db.Model, SerializerMixin):
     name = db.Column(db.String(20), unique=True, nullable=False)
     description = db.Column(db.String(200), nullable=False)
 
-    users = db.relationship('User', secondary='users_roles', backref='roles')
+    users = db.relationship('User', secondary='users_roles', back_populates='roles')
 
 class Course(db.Model, SerializerMixin):
     __tablename__ = 'courses'
@@ -38,10 +38,13 @@ class Course(db.Model, SerializerMixin):
     discipline_id = db.Column(db.Integer, db.ForeignKey('disciplines.id'), nullable=False)
     level_id = db.Column(db.Integer, db.ForeignKey('levels.id'), nullable=False)
 
-    users = db.relationship('User', secondary='users_courses', backref='courses')
-    students = db.relationship('Student', secondary='students_courses', backref='courses')
-    discipline = db.relationship('Discipline', backref='courses')
-    level = db.relationship('Level', backref='courses')
+    users = db.relationship('User', secondary='users_courses', back_populates='courses')
+    students = db.relationship('Student', secondary='students_courses', back_populates='courses')
+    discipline = db.relationship('Discipline', back_populates='courses')
+    level = db.relationship('Level', back_populates='courses')
+    course_reports = db.relationship('CourseReport', back_populates='course')
+    student_reports = db.relationship('StudentReport', back_populates='course')
+    placements = db.relationship('Placement', back_populates='course')
 
 class Discipline(db.Model, SerializerMixin):
     __tablename__ = 'disciplines'
@@ -49,8 +52,7 @@ class Discipline(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
 
-    students = db.relationship('Student', secondary='students_courses', backref='discipline')
-    courses = db.relationship('Course', secondary='users_courses', backref='discipline')
+    courses = db.relationship('Course', back_populates='discipline')
 
 class Level(db.Model, SerializerMixin):
     __tablename__ = 'levels'
@@ -58,7 +60,7 @@ class Level(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
 
-    courses = db.relationship('Course', secondary='users_courses', backref='level')
+    courses = db.relationship('Course', back_populates='level')
 
 class Student(db.Model, SerializerMixin):
     __tablename__ = 'students'
@@ -71,11 +73,11 @@ class Student(db.Model, SerializerMixin):
     birth_date = db.Column(db.Date, nullable=False)
     gender_id = db.Column(db.Integer, db.ForeignKey('genders.id'), nullable=True)
 
-    courses = db.relationship('Course', secondary='students_courses', backref='students')
-    disciplines = db.relationship('Discipline', secondary='students_courses', backref='students')
-    levels = db.relationship('Level', secondary='students_courses', backref='students')
-    reports = db.relationship('StudentReport', backref='student')
-    email = db.relationship('Emails', backref='student')
+    gender = db.relationship('Gender', back_populates='students')
+    courses = db.relationship('Course', secondary='students_courses', back_populates='students')
+    student_reports = db.relationship('StudentReport', back_populates='student')
+    email = db.relationship('Email', back_populates='student')
+    placements = db.relationship('Placement', back_populates='student')
 
 class Gender(db.Model, SerializerMixin):
     __tablename__ = 'genders'
@@ -83,7 +85,7 @@ class Gender(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
 
-    students = db.relationship('Student', secondary='students_courses', backref='gender')
+    students = db.relationship('Student', back_populates='gender')
 
 class StudentReport(db.Model, SerializerMixin):
     __tablename__ = 'student_reports'
@@ -96,9 +98,9 @@ class StudentReport(db.Model, SerializerMixin):
     date = db.Column(db.DateTime, nullable=False)
     approved = db.Column(db.Boolean, nullable=False)
 
-    student = db.relationship('Student', backref='reports')
-    course = db.relationship('Course', backref='reports')
-    user = db.relationship('User', backref='reports')
+    student = db.relationship('Student', back_populates='student_reports')
+    course = db.relationship('Course', back_populates='student_reports')
+    user = db.relationship('User', back_populates='student_reports')
 
 class CourseReport(db.Model, SerializerMixin):
     __tablename__ = 'course_reports'
@@ -110,8 +112,8 @@ class CourseReport(db.Model, SerializerMixin):
     date = db.Column(db.DateTime, nullable=False)
     approved = db.Column(db.Boolean, nullable=False)
 
-    course = db.relationship('Course', backref='reports')
-    user = db.relationship('User', backref='reports')
+    course = db.relationship('Course', back_populates='course_reports')
+    user = db.relationship('User', back_populates='course_reports')
 
 class Placement(db.Model, SerializerMixin):
     __tablename__ = 'placements'
@@ -121,8 +123,8 @@ class Placement(db.Model, SerializerMixin):
     student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
 
-    course = db.relationship('Course', backref='placements')
-    student = db.relationship('Student', backref='placements')
+    course = db.relationship('Course', back_populates='placements')
+    student = db.relationship('Student', back_populates='placements')
 
 class Suggestion(db.Model, SerializerMixin):
     __tablename__ = 'suggestions'
@@ -150,7 +152,7 @@ class Email(db.Model, SerializerMixin):
     content = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
 
-    student = db.relationship('Student', backref='emails')
+    student = db.relationship('Student', back_populates='email')
 
 
 
--- server/seed.py
+++ server/seed.py
@@ -1,12 +1,36 @@
 from config import app, db
 from models import User, Role, Course, Discipline, Level, Student, Gender, StudentReport, CourseReport, Placement, Suggestion, Template, Email
 from random import choice
+from faker import Faker
 
+fake = Faker()
 
 if __name__ == "__main__":
   with app.app_context():
     
     def create_user(first_name, last_name, email_address, password):
       user = User(first_name=first_name, last_name=last_name, email_address=email_address)
-      user.password_hash = 
+      user.password_hash = password
       db.session.add(user)
+
+    print("Clearing database...")
+    db.session.query(User).delete()
+    db.session.query(Role).delete()
+    db.session.query(Course).delete()
+    db.session.query(Discipline).delete()
+    db.session.query(Level).delete()
+    db.session.query(Student).delete()
+    db.session.query(Gender).delete()
+    db.session.query(StudentReport).delete()
+    db.session.query(CourseReport).delete()
+    db.session.query(Placement).delete()
+    db.session.query(Suggestion).delete()
+    db.session.query(Template).delete()
+    db.session.query(Email).delete()
+    db.session.commit()
+
+    print("Creating users...")
+    for i in range(10):
+      create_user(fake.first_name(), fake.last_name(), fake.email(), fake.password(length=10))
+
+    db.session.commit()
