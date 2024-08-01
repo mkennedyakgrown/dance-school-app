9fd84240c918e972f3201991d134fc2040e5ee8a
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-08-01 15:13:12 -0500

    prep for Render hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5555",
+        // target: "http://localhost:5555",
+        target:
+          "postgresql://matthewkennedy:7EwrmP1dGCw7ibjBY2jhNHSjvuY1m3HW@dpg-cqluj0aj1k6c739rvcp0-a.ohio-postgres.render.com/danceschoolserver",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
